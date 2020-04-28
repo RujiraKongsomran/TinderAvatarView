0f37671ba7e9f87c098b8d60816416b497b4fc81
Rujira Kongsomran <biwberry@outlook.com>
2020-04-28 22:42:58 +0700

    Create Interface

---
+++ app/src/main/java/com/rujirakongsomran/tinderavatarview/Interface/IFinishDrag.java
@@ -0,0 +1,9 @@
+package com.rujirakongsomran.tinderavatarview.Interface;
+
+import com.rujirakongsomran.tinderavatarview.Model.Item;
+
+import java.util.ArrayList;
+
+public interface IFinishDrag {
+    void updateListData(ArrayList<Item> list);
+}
